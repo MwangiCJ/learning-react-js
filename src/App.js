8b9accad34cfb02d4cf3a7e6c219eb6e2b4e8774
John Mwangi <jcmuthoni@safaricom.co.ke>
2021-04-20 19:50:50 +0300

    added json server for mock apis

--- src/App.js
+++ src/App.js
@@ -8,26 +8,7 @@ import { useState } from 'react'
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
 
-  const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Food shopping',
-        day: 'Feb 4th at 2:30pm',
-        reminder: false
-    },
-    {
-        id: 2,
-        text: 'Deploy application',
-        day: 'Feb 5th at 10:00am',
-        reminder: true
-    },
-    {
-        id: 3,
-        text: 'Car Wash',
-        day: 'Feb 6th at 12:20pm',
-        reminder: false
-    }
-])
+  const [tasks, setTasks] = useState([])
 
 const addTask = (task)=>{
   console.log(task)
