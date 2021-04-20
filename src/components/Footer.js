9e6defedc336fd8a72237014d4d081ae02b367ab
John Mwangi <jcmuthoni@safaricom.co.ke>
2021-04-20 21:50:04 +0300

    ready

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
 import {Link} from 'react-router-dom'
+import {useLocation} from 'react-router-dom'
+
 
 const Footer = () => {
+    const location = useLocation()
     return (
         <footer>
             <p>Copyright &copy; 2021</p>
-            <Link to='/about'>About</Link>
+            {location.pathname!=='/about' ? <Link to='/about'>About</Link> : 'About'}
         </footer>
     )
 }
